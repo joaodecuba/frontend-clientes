69e8a2d822a78d86d246be7f5a659a9c8807103d
joao paulo Lourenço <jao.ilha@gmail.com>
2021-04-27 21:05:00 -0300

    teste

---
+++ src/pages/cliente/main/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
+export default class Main extends Component {
+    constructor(props) {
+        super(props);
+ 
+        this.state = {
+            cliente: [],
+            erro: null
+        };
+    }
+ 
+    componentDidMount() {
+        fetch(`https://backend-cliente-jpl.herokuapp.com/cliente`)
+            .then(cliente =>
+                cliente.json().then(cliente => this.setState({ cliente }))
+            )
+            .catch(erro => this.setState({ erro }));
+    }
+ 
+    render() {
+        const { cliente } = this.state;
+ 
+        return (
+            <div className="cliente-list">
+                <Link to={`/criarCliente`}> <button type="button" class="btn btn-success">Novo</button> </Link>
+                <br /><br />
+ 
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Salário</th>
+                            <th scope="col">Nascimento</th>
+                            <th scope="col">Ativo</th>
+                            <th scope="col">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cliente.map((cliente, index) => (
+                            <tr>
+                                <th scope="row">{cliente.id}</th>
+                                <td>{cliente.nome}</td>
+                                <td>{cliente.salario.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+                                <td>{new Date(cliente.dataNascimento).toLocaleDateString('pt-BR', { year: 'numeric', month: '2-digit', day: '2-digit' })}</td>
+                                <td>{cliente.ativo ? "Sim" : "Não"}</td>
+                                <td> <Link to={`/clientes/${cliente.id}`}> <button type="button" class="btn btn-primary">Detalhes</button> </Link> </td>
+                                <td> <Link to={`/editarCliente/${cliente.id}`}> <button type="button" class="btn btn-warning">Atualizar</button> </Link></td>
+                                <td> <Link to={`/deletarCliente/${cliente.id}`}> <button type="button" class="btn btn-danger">Excluir</button> </Link></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
